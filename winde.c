14652c8758a42a957e6da724a2f826fab59fa1dc
Tack-12 <pranayauni1@gmail.com>
2026-09-24 12:32:56 -0500

    Getting Terminal Height with VT100 escape codes

--- winde.c
+++ winde.c
@@ -56,12 +56,31 @@ char editorReadKey(){
     return c;
 }
 
+int getCursorPosition(int *rows, int *cols){
+    char buf[32];
+    unsigned int i = 0;
+
+    if(write(STDERR_FILENO, "\x1b[6n", 4) != 4) return -1;
+
+    while( i < sizeof(buf) - 1) {
+        if( read (STDERR_FILENO, &buf[i], 1) != 1) break;
+        if (buf[i] == 'R') break;
+        i++ ;
+    }
+    buf[i] = '\0';
+    if(buf[0] != '\x1b' || buf[1] != '[') return -1;
+    if (sscanf(&buf[2], "%d;%d", rows, cols) != 2) return -1;
+
+    return 0;
+}
+
 /* Change the rows and cols using winsize from ioctl */
 int getWindowSize(int *rows, int *cols){
     struct winsize ws;
 
-    if(ioctl(STDIN_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0){
-        return -1;
+    if(1|| ioctl(STDIN_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0){
+        if(write(STDIN_FILENO, "\x1b[999C\x1b[999B,",12) != 12) return -1;
+        return getCursorPosition(rows, cols);
     } else{
         *cols = ws.ws_col;
         *rows = ws.ws_row;
@@ -88,7 +107,7 @@ void editorProcessKeyPress(){
 void editorDrawRows(){
     int y;
 
-    for (y = 0; y < 24; y++){
+    for (y = 0; y < E.screenrows ; y++){
         write(STDIN_FILENO, "~\r\n", 3);
     }
 }
