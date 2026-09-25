5b7c3be3cee22d9dd1e0ed0d1297449871bb6775
Tack-12 <pranayauni1@gmail.com>
2026-09-24 23:45:00 -0500

    Added Keybinds

--- winde.c
+++ winde.c
@@ -17,6 +17,9 @@ typedef enum {
   ARROW_RIGHT,
   ARROW_UP,
   ARROW_DOWN,
+  DELETE_KEY,
+  HOME_KEY,
+  END_KEY,
   PAGE_UP,
   PAGE_DOWN
 } editorKey;
@@ -89,10 +92,20 @@ int editorReadKey() {
           return '\x1b';
         if (seq[2] == '~') {
           switch (seq[1]) {
+          case '1':
+            return HOME_KEY;
+          case '3':
+            return DELETE_KEY;
+          case '4':
+            return END_KEY;
           case '5':
             return PAGE_UP;
           case '6':
             return PAGE_DOWN;
+          case '7':
+            return HOME_KEY;
+          case '8':
+            return END_KEY;
           }
         }
       } else {
@@ -105,9 +118,21 @@ int editorReadKey() {
           return ARROW_RIGHT;
         case 'D':
           return ARROW_LEFT;
+        case 'H':
+          return HOME_KEY;
+        case 'F':
+          return END_KEY;
         }
       }
+    } else if (seq[0] == 'O') {
+      switch (seq[1]) {
+      case 'H':
+        return HOME_KEY;
+      case 'F':
+        return END_KEY;
+      }
     }
+
     return '\x1b';
 
   } else {
@@ -199,6 +224,12 @@ void editorProcessKeyPress() {
   case CTRL_KEY('q'):
     clearScreen();
     exit(0);
+    break;
+  case HOME_KEY:
+    E.cx = 0;
+    break;
+  case END_KEY:
+    E.cx = E.screencols - 1;
     break;
 
   case PAGE_UP:
@@ -297,6 +328,5 @@ int main() {
     editorRefreshScreen();
     editorProcessKeyPress();
   }
-
   return 0;
 }
